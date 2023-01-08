e81cc0365e23f300e74475a8bdbeb7483aa72920
Boris Radulov <sc20br@leeds.ac.uk>
2023-01-09 01:05:57 +0200

    fuck yeah bby

--- Main/src/components/ProtectedRoute.js
+++ Main/src/components/ProtectedRoute.js
@@ -4,45 +4,14 @@ import { useAuth } from "../hooks/useAuth";
 
 export const ProtectedRoute = ({ children, type = "Guest" }) => {
   const { user } = useAuth();
-  const [is_logged_in, setIsLoggedIn] = React.useState(false);
-  const [userType, setType] = React.useState("Guest");
-
-  React.useEffect(() => {
-    if (user) {
-      (async () => {
-        const l_r = await fetch(`${process.env.REACT_APP_API_URL}/api/is-user-logged-in`, {
-          method: "GET",
-          headers: {
-            Authorization: `Bearer ${user.token}`,
-          },
-        });
-        const { result: is_logged_in } = await l_r.json();
-
-        setIsLoggedIn(is_logged_in);
-
-        const t_r = await fetch(`${process.env.REACT_APP_API_URL}/api/get-user-type`, {
-          method: "GET",
-          headers: {
-            Authorization: `Bearer ${user.token}`,
-          },
-        });
-        const { result: type } = await t_r.json();
-        setType(type);
-      })();
-    }
-  });
 
   if (!user) {
     return <Navigate to="/login" />;
   }
-
-  if (!is_logged_in) {
+  if (type === "Admin" && user.type !== "Admin") {
     return <Navigate to="/login" />;
   }
-  if (type === "Admin" && userType !== "Admin") {
-    return <Navigate to="/login" />;
-  }
-  if (type === "Curator" && userType !== "Admin" && userType !== "Curator") {
+  if (type === "Curator" && user.type !== "Admin" && user.type !== "Curator") {
     return <Navigate to="/login" />;
   }
 
--- Main/src/hooks/useAuth.js
+++ Main/src/hooks/useAuth.js
@@ -1,4 +1,4 @@
-import { createContext, useContext, useMemo } from "react";
+import React, { createContext, useContext, useMemo } from "react";
 import { useNavigate } from "react-router-dom";
 import { useLocalStorage } from "./useLocalStorage";
 const AuthContext = createContext();
@@ -6,6 +6,32 @@ const AuthContext = createContext();
 export const AuthProvider = ({ children }) => {
   const [user, setUser] = useLocalStorage("user", null);
   const navigate = useNavigate();
+
+  React.useEffect(() => {
+    (async () => {
+      if (user) {
+        const l_r = await fetch(`${process.env.REACT_APP_API_URL}/api/is-user-logged-in`, {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${user.token}`,
+          },
+        });
+        const { result } = await l_r.json();
+        if (!result) {
+          setUser(null);
+        } else {
+          const t_r = await fetch(`${process.env.REACT_APP_API_URL}/api/get-user-type`, {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${user.token}`,
+            },
+          });
+          const { result: type } = await t_r.json();
+          user.type = type;
+        }
+      }
+    })();
+  }, [user, setUser]);
 
   const value = useMemo(() => {
     // call this function when you want to authenticate the user
