6a2a570a70740bcb75ec3df3826b7497110d2bc1
Ognian Trajanov Jr <ogtrjr@gmail.com>
2023-01-10 18:56:28 -0500

    cimmity members

--- Main/src/pages/Dashboard/DashboardKpe.js
+++ Main/src/pages/Dashboard/DashboardKpe.js
@@ -1,13 +1,21 @@
 import SEO from "../../common/SEO";
 import HeaderAdmin from "../../common/header/HeaderAdmin";
 import Copyright from "../../common/footer/Copyright";
-import { Button, Dropdown, Input, Modal, Table } from "@nextui-org/react";
+import { Button, Dropdown, Input, Modal, Radio, Table } from "@nextui-org/react";
 import { Form } from "react-bootstrap";
 import React from "react";
+import useSWR from "swr";
+
+const fetcher = (url) =>
+  fetch(url, {
+    headers: {
+      Authorization: `Bearer ${JSON.parse(localStorage.getItem("user")).token}`,
+    },
+  }).then((res) => res.json());
 
 const columns = [
   {
-    key: "name",
+    key: "full_name",
     label: "Имена",
   },
   {
@@ -15,17 +23,13 @@ const columns = [
     label: "Имейл",
   },
   {
-    key: "chair",
+    key: "is_representative",
     label: "Председател",
   },
 ];
 
-const rows = [
-  { key: "1", name: "Цанко Спасовски", email: "office@ciab-bg.com", chair: "Да" },
-  { key: "2", name: "Иван Иванов", email: "office@ciab-bg.com", chair: "Не" },
-];
-
 const DashboardKpe = () => {
+  const { data } = useSWR(`${process.env.REACT_APP_API_URL}/api/get-ethics-committee`, fetcher);
   const [visibleAdd, setVisibleAdd] = React.useState(false);
   const [visibleEdit, setVisibleEdit] = React.useState(false);
 
@@ -33,27 +37,38 @@ const DashboardKpe = () => {
     <>
       {/* Modal add start */}
       <Modal scroll open={visibleAdd} onClose={() => setVisibleAdd(false)}>
-        <Form>
+        <Form
+          onSubmit={async (e) => {
+            e.preventDefault();
+            const body = new FormData();
+            body.append("full_name", e.target.full_name.value);
+            body.append("email", e.target.email.value);
+            body.append("is_representative", e.target.isChair.value === "chair" ? true : false);
+            const resp = await fetch(`${process.env.REACT_APP_API_URL}/api/post-ethics-committee-member`, {
+              method: "POST",
+              body: body,
+              headers: {
+                Authorization: `Bearer ${JSON.parse(localStorage.getItem("user")).token}`,
+              },
+            });
+            window.location.reload(false);
+          }}
+        >
           <Modal.Header>
             <div style={{ marginTop: 20 }}>
-              <Input placeholder="Имена" style={{ background: "white", margin: 0 }} />
+              <Input placeholder="Имена" style={{ background: "white", margin: 0 }} name="full_name" id="full_name" aria-label="Names" aria-labelledby="email" />
             </div>
           </Modal.Header>
           <Modal.Body>
             <div style={{ display: "flex", flexDirection: "column", alignSelf: "center" }}>
               <div style={{ marginTop: 20 }}>
-                <Input placeholder="Email" style={{ background: "white", margin: 0 }} />
+                <Input placeholder="Email" style={{ background: "white", margin: 0 }} name="email" id="email" aria-label="Email" aria-labelledby="email" />
               </div>
               <div style={{ marginTop: 20 }}>
-                <Dropdown>
-                  <Dropdown.Button color="warning" shadow>
-                    Председател
-                  </Dropdown.Button>
-                  <Dropdown.Menu>
-                    <Dropdown.Item key="chair">Да</Dropdown.Item>
-                    <Dropdown.Item key="notChair">Не</Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
+                <Radio.Group label="Председател" defaultValue="chair" name="isChair" id="isChair"  color="warning">
+                  <Radio value="chair">Да</Radio>
+                  <Radio value="notChair">Не</Radio>
+                </Radio.Group>
               </div>
             </div>
           </Modal.Body>
@@ -71,13 +86,13 @@ const DashboardKpe = () => {
         <Form>
           <Modal.Header>
             <div style={{ marginTop: 20 }}>
-              <Input placeholder="Имена" style={{ background: "white", margin: 0 }} />
+              <Input placeholder="Имена" style={{ background: "white", margin: 0 }} aria-label="Email" aria-labelledby="email" />
             </div>
           </Modal.Header>
           <Modal.Body>
             <div style={{ display: "flex", flexDirection: "column", alignSelf: "center" }}>
               <div style={{ marginTop: 20 }}>
-                <Input placeholder="Email" style={{ background: "white", margin: 0 }} />
+                <Input placeholder="Email" style={{ background: "white", margin: 0 }} aria-label="Email" aria-labelledby="email" />
               </div>
               <div style={{ marginTop: 20 }}>
                 <Dropdown>
@@ -111,8 +126,8 @@ const DashboardKpe = () => {
           style={{
             display: "flex",
             flexDirection: "column",
+            minHeight: "90vh",
             alignItems: "center",
-            height: "90vh",
             justifyContent: "center",
           }}
         >
@@ -120,46 +135,48 @@ const DashboardKpe = () => {
             Добавете член
           </Button>
           <div style={{ display: "flex", width: "100%", justifyContent: "center", marginTop: 50, marginBottom: 50 }}>
-            <Table
-              css={{
-                height: "auto",
-                minWidth: "100%",
-              }}
-            >
-              <Table.Header columns={columns}>
-                {(column) => (
-                  <Table.Column key={column.key}>
-                    <span style={{ fontSize: 14, marginLeft: 5, marginRight: 5 }}>{column.label}</span>
-                  </Table.Column>
-                )}
-              </Table.Header>
-              <Table.Body items={rows}>
-                {(item) => (
-                  <Table.Row key={item.key}>
-                    {(columnKey) => (
-                      <Table.Cell>
-                        <span
-                          style={{ cursor: "pointer" }}
-                          onClick={() => {
-                            setVisibleEdit(true);
-                          }}
-                        >
-                          {item.chair === "Не" ? (
-                            <span style={{ color: "black", fontSize: 14, fontWeight: "normal" }}>
-                              {item[columnKey]}
-                            </span>
-                          ) : (
-                            <span style={{ color: "orange", fontSize: 14, fontWeight: "normal" }}>
-                              {item[columnKey]}
-                            </span>
-                          )}
-                        </span>
-                      </Table.Cell>
-                    )}
-                  </Table.Row>
-                )}
-              </Table.Body>
-            </Table>
+            {data &&
+              <Table
+                css={{
+                  height: "auto",
+                  minWidth: "100%",
+                }}
+              >
+                <Table.Header columns={columns}>
+                  {(column) => (
+                    <Table.Column key={column.key}>
+                      <span style={{ fontSize: 14, marginLeft: 5, marginRight: 5 }}>{column.label}</span>
+                    </Table.Column>
+                  )}
+                </Table.Header>
+                <Table.Body items={data.results}>
+                  {(item) => (
+                    <Table.Row key={(item._id)}>
+                      {(columnKey) => (
+                        <Table.Cell>
+                          <span
+                            style={{ cursor: "pointer" }}
+                            onClick={() => {
+                              setVisibleEdit(true);
+                            }}
+                          >
+                            {item.is_representative === false ? (
+                              <span style={{ color: "black", fontSize: 14, fontWeight: "normal" }}>
+                                {item[columnKey] !== false ? item[columnKey] : "Не"}
+                              </span>
+                            ) : (
+                              <span style={{ color: "orange", fontSize: 14, fontWeight: "normal" }}>
+                                {item[columnKey] !== true ? item[columnKey] : "Да"}
+                              </span>
+                            )}
+                          </span>
+                        </Table.Cell>
+                      )}
+                    </Table.Row>
+                  )}
+                </Table.Body>
+              </Table>
+            }
           </div>
         </div>
         <Copyright />
--- Main/src/pages/Dashboard/DashboardRc.js
+++ Main/src/pages/Dashboard/DashboardRc.js
@@ -1,221 +1,100 @@
 import SEO from "../../common/SEO";
 import HeaderAdmin from "../../common/header/HeaderAdmin";
 import Copyright from "../../common/footer/Copyright";
-import { Button, Dropdown, Input, Modal, Table } from "@nextui-org/react";
+import { Button, Dropdown, Input, Modal, Radio, Table } from "@nextui-org/react";
 import { Form } from "react-bootstrap";
 import React from "react";
+import useSWR from "swr";
+import RCTable from "./RCTable";
 
-const columns = [
-  {
-    key: "name",
-    label: "Имена",
-  },
-  {
-    key: "email",
-    label: "Имейл",
-  },
-  {
-    key: "chair",
-    label: "Председател",
-  },
-];
-
-const rowsBurgas = [
-  { key: "1", name: "Цанко Спасовски", email: "office@ciab-bg.com", chair: "Да" },
-  { key: "2", name: "Иван Иванов", email: "office@ciab-bg.com", chair: "Не" },
-];
-
-const rowsVarna = [
-  { key: "1", name: "Цанко Спасовски", email: "office@ciab-bg.com", chair: "Да" },
-  { key: "2", name: "Иван Иванов", email: "office@ciab-bg.com", chair: "Не" },
-];
-
-const rowsPleven = [
-  { key: "1", name: "Цанко Спасовски", email: "office@ciab-bg.com", chair: "Да" },
-  { key: "2", name: "Иван Иванов", email: "office@ciab-bg.com", chair: "Не" },
-];
-
-const rowsPlovdiv = [
-  { key: "1", name: "Цанко Спасовски", email: "office@ciab-bg.com", chair: "Да" },
-  { key: "2", name: "Иван Иванов", email: "office@ciab-bg.com", chair: "Не" },
-];
-
-const rowsRuse = [
-  { key: "1", name: "Цанко Спасовски", email: "office@ciab-bg.com", chair: "Да" },
-  { key: "2", name: "Иван Иванов", email: "office@ciab-bg.com", chair: "Не" },
-];
-
-const rowsSofia = [
-  { key: "1", name: "Цанко Спасовски", email: "office@ciab-bg.com", chair: "Да" },
-  { key: "2", name: "Иван Иванов", email: "office@ciab-bg.com", chair: "Не" },
-];
-
-const rowsStaraZagora = [
-  { key: "1", name: "Цанко Спасовски", email: "office@ciab-bg.com", chair: "Да" },
-  { key: "2", name: "Иван Иванов", email: "office@ciab-bg.com", chair: "Не" },
-];
-
-const rowsShumen = [
-  { key: "1", name: "Цанко Спасовски", email: "office@ciab-bg.com", chair: "Да" },
-  { key: "2", name: "Иван Иванов", email: "office@ciab-bg.com", chair: "Не" },
-];
-
-const regions = [
-  { key: 1, name: "Бургас", info: rowsBurgas },
-  { key: 2, name: "Варна", info: rowsVarna },
-  { key: 3, name: "Плевен", info: rowsPleven },
-  { key: 4, name: "Пловдив", info: rowsPlovdiv },
-  { key: 4, name: "Русе", info: rowsRuse },
-  { key: 5, name: "Стара Загора", info: rowsStaraZagora },
-  { key: 6, name: "София", info: rowsSofia },
-  { key: 7, name: "Шумен", info: rowsShumen },
-];
+const fetcher = (url) => fetch(url).then((res) => res.json());
 
 const DashboardRc = () => {
-  const [visibleAdd, setVisibleAdd] = React.useState(false);
-  const [visibleEdit, setVisibleEdit] = React.useState(false);
+  const { data: sofia } = useSWR(`${process.env.REACT_APP_API_URL}/api/get-regional-committee?city=Sofia`, fetcher);
+  const { data: burgas } = useSWR(`${process.env.REACT_APP_API_URL}/api/get-regional-committee?city=Burgas`, fetcher);
+  const { data: varna } = useSWR(`${process.env.REACT_APP_API_URL}/api/get-regional-committee?city=Varna`, fetcher);
+  const { data: pleven } = useSWR(`${process.env.REACT_APP_API_URL}/api/get-regional-committee?city=Pleven`, fetcher);
+  const { data: plovdiv } = useSWR(`${process.env.REACT_APP_API_URL}/api/get-regional-committee?city=Plovdiv`, fetcher);
+  const { data: ruse } = useSWR(`${process.env.REACT_APP_API_URL}/api/get-regional-committee?city=Ruse`, fetcher);
+  const { data: zagora } = useSWR(`${process.env.REACT_APP_API_URL}/api/get-regional-committee?city=Zagora`, fetcher);
+  const { data: shumen } = useSWR(`${process.env.REACT_APP_API_URL}/api/get-regional-committee?city=Tutrakan`, fetcher);
 
+  const [visibleAdd, setVisibleAdd] = React.useState(false);
   return (
     <>
-      {/* Modal add start */}
-      <Modal scroll open={visibleAdd} onClose={() => setVisibleAdd(false)}>
-        <Form>
-          <Modal.Header>
-            <div style={{ marginTop: 20 }}>
-              <Input placeholder="Имена" style={{ background: "white", margin: 0 }} />
-            </div>
-          </Modal.Header>
-          <Modal.Body>
-            <div style={{ display: "flex", flexDirection: "column", alignSelf: "center" }}>
-              <div style={{ marginTop: 20 }}>
-                <Input placeholder="Email" style={{ background: "white", margin: 0 }} />
-              </div>
-              <div style={{ marginTop: 20 }}>
-                <Dropdown>
-                  <Dropdown.Button color="warning" shadow>
-                    Председател
-                  </Dropdown.Button>
-                  <Dropdown.Menu>
-                    <Dropdown.Item key="chair">Да</Dropdown.Item>
-                    <Dropdown.Item key="notChair">Не</Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
-              </div>
-            </div>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button auto type="submit" color="warning">
-              Качи
-            </Button>
-          </Modal.Footer>
-        </Form>
-      </Modal>
-      {/* Modal add end */}
+      {
+        (<>
+          {/* Modal add start */}
+          <Modal scroll open={visibleAdd} onClose={() => setVisibleAdd(false)}>
+            <Form onSubmit={async (e) => {
+              e.preventDefault();
+              const body = new FormData();
+              body.append("full_name", e.target.full_name.value);
+              body.append("email", e.target.email.value);
+              body.append("is_representative", e.target.isChair.value === "chair" ? true : false);
+              body.append("city", e.target.city.value)
+              const resp = await fetch(`${process.env.REACT_APP_API_URL}/api/post-regional-committee-member`, {
+                method: "POST",
+                body: body,
+                headers: {
+                  Authorization: `Bearer ${JSON.parse(localStorage.getItem("user")).token}`,
+                },
+              });
+              window.location.reload(false);
+            }}>
+              <Modal.Header>
+                <div style={{ marginTop: 20 }}>
+                  <Input placeholder="Имена" style={{ background: "white", margin: 0 }} id="full_name" name="full_name" />
+                </div>
+              </Modal.Header>
+              <Modal.Body>
+                <div style={{ display: "flex", flexDirection: "column", alignSelf: "center" }}>
+                  <div style={{ marginTop: 20 }}>
+                    <Input placeholder="Email" style={{ background: "white", margin: 0 }} id="email" name="email" />
+                  </div>
+                  <div style={{ marginTop: 20 }}>
+                    <Radio.Group label="Град" defaultValue="chair" name="city" id="city" color="warning">
+                      <Radio value="Burgas">Бургас</Radio>
+                      <Radio value="Varna">Варна</Radio>
+                      <Radio value="Pleven">Плевен</Radio>
+                      <Radio value="Plovdiv">Пловдив</Radio>
+                      <Radio value="Ruse">Русе</Radio>
+                      <Radio value="Sofia">София</Radio>
+                      <Radio value="Zagora">Стара Загора</Radio>
+                      <Radio value="Tutrakan">Шумен</Radio>
+                    </Radio.Group>
+                  </div>
+                  <div style={{ marginTop: 20 }}>
+                    <Radio.Group label="Председател" defaultValue="chair" name="isChair" id="isChair" color="warning">
+                      <Radio value="chair">Да</Radio>
+                      <Radio value="notChair">Не</Radio>
+                    </Radio.Group>
+                  </div>
+                </div>
+              </Modal.Body>
+              <Modal.Footer>
+                <Button auto type="submit" color="warning">
+                  Качи
+                </Button>
+              </Modal.Footer>
+            </Form>
+          </Modal>
+          {/* Modal add end */}
 
-      {/* Modal edit start */}
-      <Modal scroll open={visibleEdit} onClose={() => setVisibleEdit(false)}>
-        <Form>
-          <Modal.Header>
-            <div style={{ marginTop: 20 }}>
-              <Input placeholder="Имена" style={{ background: "white", margin: 0 }} />
-            </div>
-          </Modal.Header>
-          <Modal.Body>
-            <div style={{ display: "flex", flexDirection: "column", alignSelf: "center" }}>
-              <div style={{ marginTop: 20 }}>
-                <Input placeholder="Email" style={{ background: "white", margin: 0 }} />
-              </div>
-              <div style={{ marginTop: 20 }}>
-                <Dropdown>
-                  <Dropdown.Button color="warning" shadow>
-                    Председател
-                  </Dropdown.Button>
-                  <Dropdown.Menu>
-                    <Dropdown.Item key="chair">Да</Dropdown.Item>
-                    <Dropdown.Item key="notChair">Не</Dropdown.Item>
-                  </Dropdown.Menu>
-                </Dropdown>
-              </div>
-            </div>
-          </Modal.Body>
-          <Modal.Footer>
-            <Button auto type="submit" color="error">
-              Изтрий
-            </Button>
-            <Button auto type="submit" color="warning">
-              Запази
-            </Button>
-          </Modal.Footer>
-        </Form>
-      </Modal>
-      {/* Modal edit end */}
-
-      <SEO title="Административен панел" />
-      <main className="page-wrapper">
-        <HeaderAdmin btnStyle="btn-small round btn-icon" />
-        <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
-          <Button size="xl" color="warning" style={{ width: 200, marginTop: 50 }} onPress={() => setVisibleAdd(true)}>
-            Добавете член
-          </Button>
-          <div style={{ display: "flex", flexDirection: "column", width: "100%", justifyContent: "center" }}>
-            {regions.map((item) => (
-              <div
-                style={{
-                  display: "flex",
-                  flexDirection: "column",
-                  marginLeft: 100,
-                  marginRight: 100,
-                  marginBottom: 50,
-                }}
-              >
-                <h3>{item.name}</h3>
-                <Table
-                  css={{
-                    height: "auto",
-                    minWidth: "50%",
-                  }}
-                >
-                  <Table.Header columns={columns}>
-                    {(column) => (
-                      <Table.Column key={column.key}>
-                        <span style={{ fontSize: 14, marginLeft: 5, marginRight: 5 }}>{column.label}</span>
-                      </Table.Column>
-                    )}
-                  </Table.Header>
-                  <Table.Body items={item.info}>
-                    {(item) => (
-                      <Table.Row key={item.key}>
-                        {(columnKey) => (
-                          <Table.Cell>
-                            <span
-                              style={{ cursor: "pointer" }}
-                              onClick={() => {
-                                setVisibleEdit(true);
-                              }}
-                            >
-                              {item.chair === "Не" ? (
-                                <span style={{ color: "black", fontSize: 14, fontWeight: "normal" }}>
-                                  {item[columnKey]}
-                                </span>
-                              ) : (
-                                <span style={{ color: "orange", fontSize: 14, fontWeight: "normal" }}>
-                                  {item[columnKey]}
-                                </span>
-                              )}
-                            </span>
-                          </Table.Cell>
-                        )}
-                      </Table.Row>
-                    )}
-                  </Table.Body>
-                </Table>
-              </div>
-            ))}
-          </div>
-        </div>
-        <Copyright />
-      </main>
-    </>
+          <SEO title="Административен панел" />
+          <main className="page-wrapper">
+            <HeaderAdmin btnStyle="btn-small round btn-icon" />
+            <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
+              <Button size="xl" color="warning" style={{ width: 200, marginTop: 50 }} onPress={() => setVisibleAdd(true)}>
+                Добавете член
+              </Button>
+              {
+                sofia && shumen && burgas && varna && pleven && plovdiv && ruse && zagora &&
+                <RCTable sofia={sofia} burgas={burgas} varna={varna} ruse={ruse} pleven={pleven} plovdiv={plovdiv} zagora={zagora} shumen={shumen} />
+              }</div>
+            <Copyright />
+          </main>
+        </>)}</>
   );
 };
 export default DashboardRc;
